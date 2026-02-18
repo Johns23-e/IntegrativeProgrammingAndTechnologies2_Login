d3012dd40b87aadb8b0d6c9952d675d19cda95da
kianchris <kianchris.gio@evsu.edu.ph>
2026-02-18 20:14:05 +0800

    MY LOGIN FORM

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('My-Login');
+
+  username = '';
+  password = '';
+
+  message = '';
+  error = '';
+
+  correctUsername = 'JOHN ROQUE ABINA';
+  correctPassword = '2023-00145';
+
+  onLogin() {
+    if (
+      this.username.trim().toUpperCase() === this.correctUsername &&
+      this.password.trim() === this.correctPassword
+    ) {
+      this.error = '';
+      this.message = 'Hello, Abina, John Roque B. WELCOME TO IntegrativeProgrammingAndTechnologies 2. Have fun to learn!';
+    } else {
+      this.message = '';
+      this.error = 'Invalid username or password. Please try again.';
+    }
+  }
 }
